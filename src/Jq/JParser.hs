a24d5951ffe6591ce1a14f6064196e4be54d5241
mstyczen <42466994+mstyczen@users.noreply.github.com>
2021-03-31 15:02:15 +0200

    Parser (mostly) done

--- src/Jq/JParser.hs
+++ src/Jq/JParser.hs
@@ -3,9 +3,94 @@ module Jq.JParser where
 import Parsing.Parsing
 import Jq.Json
 
+-- define parsing for each
 parseJNull :: Parser JSON
-parseJNull = do _ <- string "null"
-                return JNull
+parseJNull = do 
+    _ <- string "null"
+    return JNull
+
+-- parseJString :: Parser JSON
+parseJString :: Parser JSON
+parseJString = do
+    s <- parseString
+    return (JString s)
+
+parseString :: Parser String 
+parseString = do
+    _ <- symbol "\""
+    s <- many (sat (/= '"'))
+    _ <- symbol "\""
+    return s
+
+parseJBoolean :: Parser JSON 
+parseJBoolean = do 
+    x <- string "false" <|> string "true"
+    if x == "false" then return (JBoolean False) else return (JBoolean True)
+
+parseJNumber :: Parser JSON 
+parseJNumber = parseJScientific <|> parseJStandard <|> parseJInt
+
+parseJInt :: Parser JSON 
+parseJInt = do
+    x <- int
+    return (JNumber (fromIntegral x))
+
+parseJStandard :: Parser JSON 
+parseJStandard = do
+    l <- int 
+    _ <- symbol "."
+    r <- some digit
+    return (JNumber (read (show l ++ "." ++ r)))
+
+parseJScientific :: Parser JSON
+parseJScientific = do
+    l <- int 
+    _ <- char '.'
+    r <- int
+    _ <- symbol "e" <|> symbol "E"
+    sign <- symbol "+" <|> symbol "-"
+    e <- int
+    return (JNumber (read (show l ++ "." ++ show r ++ "e" ++ sign ++ show e)))
+
+parseJArray :: Parser JSON
+parseJArray = parseNonEmptyJArray 
+-- <|> parseEmptyJArray
+
+parseNonEmptyJArray :: Parser JSON
+parseNonEmptyJArray = do
+    _ <- symbol "["
+    n <- parseJSON
+    ns <- many (do 
+        _ <- symbol "," 
+        parseJSON)
+    _ <- symbol "]"
+    return (JArray (n:ns))
+
+-- parseEmptyJArray :: Parser b
+-- parseEmptyJArray = ???
+
+parseJObj :: Parser JSON 
+parseJObj = parseNonEmptyJObj
+-- <|> parseEmptyJObj
+
+parseNonEmptyJObj :: Parser JSON 
+parseNonEmptyJObj = do
+    _ <- symbol "{"
+    n <- parseKV
+    ns <- many (do
+        _ <- symbol ","
+        parseKV)
+    _ <- symbol "}"
+    return (JObj (n:ns))
+
+parseKV :: Parser (String, JSON)
+parseKV = do
+        k <- parseString
+        _ <- symbol ":"
+        v <- parseJSON
+        return (k,v)
+
+-- parseEmptyJObj
 
 parseJSON :: Parser JSON
-parseJSON = token $ parseJNull
+parseJSON = token $ parseJNull <|> parseJBoolean <|> parseJString <|> parseJNumber <|> parseJArray <|> parseJObj
--- src/Jq/Json.hs
+++ src/Jq/Json.hs
@@ -1,7 +1,21 @@
 module Jq.Json where
 
+import Data.List
+
 data JSON =
-    JNull
+    JNull | JNumber Float | JString String | JBoolean Bool | JObj [(String, JSON)] | JArray [JSON] 
 
 instance Show JSON where
-  show (JNull) = "null"
+  -- adapted from "Pretty printing JSON data" exercise on weblab
+  show JNull = "null"
+  show (JNumber x) = show x
+  show (JString x) = show x
+  show (JBoolean True) = "true"
+  show (JBoolean False) = "false"
+  show (JArray x) = "[" ++ values x ++ "]" where
+    values [] = ""
+    values xs = intercalate ", " (map show xs)
+
+  show (JObj x) = "{"++ renderList x ++ "}" where
+    renderList ys = intercalate ", " ([show (fst y) ++ ": " ++ show (snd y) | y <- ys])
+
